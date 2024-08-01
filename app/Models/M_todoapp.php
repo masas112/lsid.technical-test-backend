91877ce6c4167fe165e1834fd607e16db9ec7cbb
Muhamad Asrori <muhammad.asrori112@gmail.com>
2024-08-01 20:42:53 +0700

    Setting Models

---
+++ app/Models/M_todoapp.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class M_todoapp extends Model
+{
+    use HasFactory;
+
+    protected $table = "todoapp";
+    protected $primaryKey = "id";
+    protected $fillable = [
+        "title",
+        "description",
+        "status",
+    ];
+}
